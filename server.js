9981d32c19262ac48e38f792a15362ba5f611111
Zemian Deng <zemian.deng@oracle.com>
2020-10-17 12:26:53 -0400

    Dynamic date!

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 app.use(express.static('web'));
+app.get('/hello', (req, res) => {
+  console.log(req);
+  res.send('hi ' + new Date());
+});
 app.listen(8000, () => {
   console.log('Server is ready');
 });
