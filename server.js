d695f8f09eac80bd6689385919aae6078c0fc575
Kenny Deng <kennydeng352@gmail.com>
2020-10-17 18:50:46 -0400

    Add User input in URL

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 app.use(express.static('web'));
 app.get('/hello', (req, res) => {
-  console.log(req);
   res.send('hi ' + new Date());
+});
+app.get('/dirlist', (req, res) => {
+	let inputdir = req.query.dir;
+	fs.readdir(inputdir, (err, files) => {
+		let fileList = '';
+		files.forEach((file) => {
+			fileList += `<li>${file}</li>`;
+		});
+		res.send(
+			`<!DOCTYPE html>
+				<html>
+				<head>
+				  <title></title>
+				</head>
+				<body>
+					<ul>
+						${fileList}
+					</ul>
+				</body>
+				</html>
+			`);
+	});
 });
 app.listen(8000, () => {
   console.log('Server is ready');
